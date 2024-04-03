81810d12ed4b183284bc3e8437fc6bae3770f219
Arnab Ghosh <arnabbabon2002@gmail.com>
2024-04-03 08:47:51 +0530

    all deleted

--- src/constants.js
+++
@@ -1,6 +0,0 @@
-export const DB_NAME = "VidVerse";
-
-export const CLOUD_AVATAR_FOLDER_NAME = "VidVerse/Avatar";
-export const CLOUD_COVERIMAGE_FOLDER_NAME = "VidVerse/CoverImage";
-export const CLOUD_VIDEO_FOLDER_NAME = "VidVerse/Video";
-export const CLOUD_THUMBNAIL_FOLDER_NAME = "VidVerse/Thumbnail";
--- src/controllers/healthcheck.controller.js
+++
@@ -1,8 +0,0 @@
-import { ApiResponse } from "../utils/ApiResponse.util.js";
-import { asyncHandler } from "../utils/asyncHandler.util.js";
-
-const healthcheck = asyncHandler(async (req, res) => {
-  res.status(200).json(new ApiResponse(200, {}, "System is working fine"));
-});
-
-export { healthcheck };
