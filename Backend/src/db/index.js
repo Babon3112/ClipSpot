8a4774f7214858bf4a5c9250d66055166498a905
Arnab Ghosh <arnabbabon2002@gmail.com>
2024-04-07 16:16:45 +0530

    small fixes

--- Backend/src/db/index.js
+++ Backend/src/db/index.js
@@ -7,34 +7,12 @@ const connectDB = async () => {
       `${process.env.MONGODB_URI}/${DB_NAME}`
     );
     console.log(
-      `\n MONGODB connected !! DB HOST: ${connectionInstance.connection.host}`
+      `\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`
     );
   } catch (error) {
-    console.log("MONGODB connection FAILED", error);
+    console.log("MONGODB connection FAILED ", error);
     process.exit(1);
   }
 };
 
 export default connectDB;
-
-
-/*
-import express from "express";
-const app = express();
-
-(async () => {
-  try {
-    await mongoose.connect(`${process.envMONGODB_URL}/${DB_NAME}`);
-    app.on("Error", (error) => {
-      console.log(error);
-      throw error;
-    });
-
-    app.listen(process.env.PORT, () => {
-      console.log(`App is listening on ${process.env.PORT}`);
-    });
-  } catch (error) {
-    console.log("Error: " + error);
-  }
-})();
-*/
--- Backend/src/server.js
+++ Backend/src/server.js
@@ -17,3 +17,25 @@ connectDB()
   .catch((err) => {
     console.log("MONGODB connection failed !!! ", err);
   });
+
+
+  /*
+import express from "express";
+const app = express();
+
+(async () => {
+  try {
+    await mongoose.connect(`${process.envMONGODB_URL}/${DB_NAME}`);
+    app.on("Error", (error) => {
+      console.log(error);
+      throw error;
+    });
+
+    app.listen(process.env.PORT, () => {
+      console.log(`App is listening on ${process.env.PORT}`);
+    });
+  } catch (error) {
+    console.log("Error: " + error);
+  }
+})();
+*/
